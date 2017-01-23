ad2eae8db5960f320791f42286b450b17076b8b9
domos43 <domos43@gmail.com>
2017-01-23 12:17:12 +0100

    Add files via upload

---
+++ slot.js
@@ -0,0 +1,215 @@
+
+$(document).ready(function() {
+    /**
+    * zmienne 
+    */
+    var completed = 0,
+        imgHeight = 1374,
+        posArr = [
+            0, //pomarancz
+            80, //7 
+            165, //bar
+            237, //gruszka
+            310, //banan
+            378, //wisnia
+            454, //pomarancz
+            539, //7
+            624, //bar
+            696, //gruszka
+            769, //banan
+            837, //wisnia
+            913, //pomarancz
+            1000, //7
+            1085, //bar
+            1157, //gruszka
+            1230, //banan
+            1298 //wisnia
+        ];
+    
+    var win = [];
+    win[0] = win[454] = win[913] = 1;
+    win[80] = win[539] = win[1000] = 2;
+    win[165] = win[624] = win[1085] = 3;
+    win[237] = win[696] = win[1157] = 4;
+    win[310] = win[769] = win[1230] = 5;
+    win[378] = win[837] = win[1298] = 6;
+
+    /**
+    *  Slot
+    */
+    function Slot(el, max, step) {
+        this.speed = 0; 
+        this.step = step; 
+        this.si = null; 
+        this.el = el; 
+        this.maxSpeed = max; 
+        this.pos = null;     
+
+        $(el).pan({
+            fps:30,
+            dir:'down'
+        });
+        $(el).spStop();
+    }
+
+    /**
+    * start
+    */
+    Slot.prototype.start = function() {
+        var _this = this;
+        $(_this.el).addClass('motion');
+        $(_this.el).spStart();
+        _this.si = window.setInterval(function() {
+            if(_this.speed < _this.maxSpeed) {
+                _this.speed += _this.step;
+                $(_this.el).spSpeed(_this.speed);
+            }
+        }, 100);
+    };
+
+    /**
+    * stop
+    */
+    Slot.prototype.stop = function() {
+        var _this = this,
+            limit = 30;
+        clearInterval(_this.si);
+        _this.si = window.setInterval(function() {
+            if(_this.speed > limit) {
+                _this.speed -= _this.step;
+                $(_this.el).spSpeed(_this.speed);
+            }
+            if(_this.speed <= limit) {
+                _this.finalPos(_this.el);
+                $(_this.el).spSpeed(0);
+                $(_this.el).spStop();
+                clearInterval(_this.si);
+                $(_this.el).removeClass('motion');
+                _this.speed = 0;
+            }
+        }, 100);
+    };
+
+    /**
+    * finalPos
+    */
+    Slot.prototype.finalPos = function() {
+        var el = this.el,
+            el_id,
+            pos,
+            posMin = 2000000000,
+            best,
+            bgPos,
+            i,
+            j,
+            k;
+
+        el_id = $(el).attr('id');
+        pos = document.getElementById(el_id).style.backgroundPosition;
+        pos = pos.split(' ')[1];
+        pos = parseInt(pos, 10);
+
+        for(i = 0; i < posArr.length; i++) {
+            for(j = 0;;j++) {
+                k = posArr[i] + (imgHeight * j);
+                if(k > pos) {
+                    if((k - pos) < posMin) {
+                        posMin = k - pos;
+                        best = k;
+                        this.pos = posArr[i]; 
+                    }
+                    break;
+                }
+            }
+        }
+
+        best += imgHeight + 4;
+        bgPos = "0 " + best + "px";
+        $(el).animate({
+            backgroundPosition:"(" + bgPos + ")"
+        }, {
+            duration: 200,
+            complete: function() {
+                completed ++;
+            }
+        });
+    };
+    
+    /**
+    * reset
+    */
+    Slot.prototype.reset = function() {
+        var el_id = $(this.el).attr('id');
+        $._spritely.instances[el_id].t = 0;
+        $(this.el).css('background-position', '0px 4px');
+        this.speed = 0;
+        completed = 0;
+        $('#result').html('');
+    };
+
+    function enableControl() {
+        $('#control').attr("disabled", false);
+    }
+
+    function disableControl() {
+        $('#control').attr("disabled", true);
+    }
+
+    function printResult() {
+        var res;
+        if(win[a.pos] === win[b.pos] && win[a.pos] === win[c.pos]) {
+            res = "You Win!";
+        } else {
+            res = "You Lose";
+        }
+        $('#result').html(res);
+    }
+
+    var a = new Slot('#slot1', 30, 1),
+        b = new Slot('#slot2', 45, 2),
+        c = new Slot('#slot3', 70, 3);
+
+    /**
+    * kontrola Slotu 
+    */
+    $('#control').click(function() {
+        var x;
+        if(this.innerHTML == "Start") {
+            a.start();
+            b.start();
+            c.start();
+            this.innerHTML = "Stop";
+            
+            disableControl(); 
+            
+            
+            x = window.setInterval(function() {
+                if(a.speed >= a.maxSpeed && b.speed >= b.maxSpeed && c.speed >= c.maxSpeed) {
+                    enableControl();
+                    window.clearInterval(x);
+                }
+            }, 100);
+        } else if(this.innerHTML == "Stop") {
+            a.stop();
+            b.stop();
+            c.stop();
+            this.innerHTML = "Reset";
+
+            disableControl(); 
+            
+           
+            x = window.setInterval(function() {
+                if(a.speed === 0 && b.speed === 0 && c.speed === 0 && completed === 3) {
+                    enableControl();
+                    window.clearInterval(x);
+                    printResult();
+                }
+            }, 100);
+        } else { //reset
+            a.reset();
+            b.reset();
+            c.reset();
+            this.innerHTML = "Start";
+        }
+    });
+});
